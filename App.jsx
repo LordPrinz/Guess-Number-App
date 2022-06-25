6438b6cbe8ab946843c0f998d66393982123af92
LordPrinz <plazinski2902@gmail.com>
2022-06-25 15:43:03 +0200

    Add buttons to the component

--- App.jsx
+++ App.jsx
@@ -1,13 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Hello World!!!!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+	return <StartGameScreen />;
 }
 
 const styles = StyleSheet.create({
