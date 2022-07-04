a2c7224d7966cf31dd595344df5ed76a7851ed66
LordPrinz <plazinski2902@gmail.com>
2022-07-04 15:17:58 +0200

    Add generate random between function

---
+++ utils/generateRandomBetween.js
@@ -0,0 +1,11 @@
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+}
+
+export default generateRandomBetween;
